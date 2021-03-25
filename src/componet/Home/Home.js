0dea9732071ea214cdae5489830dcf8a45e35573
csjv95 <csjv95@gmail.com>
2021-03-25 15:25:54 +0900

    get user data on home route

--- src/componet/Home/Home.js
+++ src/componet/Home/Home.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StMainRouterSection } from "../../Global/StMainRouterSection/StMainRouterSection";
-
+import homeData from "../../service/routeData/homeData";
 
 const Home = () => {
-  
+  const [userData, setUserData] = useState({});
 
+  useEffect(() => {
+    homeData(setUserData);
+  }, []);
+  
   return (
     <StMainRouterSection>
       <h1>Home</h1>
-      
+      {userData.nicname}
     </StMainRouterSection>
   );
 };
--- src/service/routeData/homeData.js
+++ src/service/routeData/homeData.js
@@ -1,29 +1,27 @@
 import { firebasestore } from "../firebase";
 import { firebaseAuth } from "../firebase";
 
-const homeData =  (setNicName) => {
-   firebaseAuth.onAuthStateChanged((user) => {
-    if (user) {
-      const uid = user.uid;
-      console.log("uid", uid);
+const homeData = (setUserData) => {
+  const user = firebaseAuth.currentUser;
+  const uid = user.uid;
 
-      const docRef = firebasestore.collection("users").doc(uid);
+  const docRef = firebasestore.collection("users").doc(uid);
 
-      docRef
-        .get()
-        .then((doc) => {
-          if (doc.exists) {
-            console.log("Document data:", doc.data());
-            setNicName(doc.data());
-          } 
-        })
-        .catch((error) => {
-          console.log("Error getting document:", error);
-        });
-    } else {
-      console.log("??");
-    }
-  });
+  docRef
+    .get()
+    .then((doc) => {
+      console.log(doc.exists);
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+        setUserData(doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+    });
 };
 
 export default homeData;
