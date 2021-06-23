26dd77b145ef37924638df10d888b0bb66fff8a2
csjv95 <csjv95@gmail.com>
2021-06-23 19:48:29 +0900

    styling PostMenu and add function delete and cancel

--- src/componet/PostMenu/PostMenu.js
+++ src/componet/PostMenu/PostMenu.js
@@ -1,17 +1,70 @@
 import React from "react";
+import styled from "styled-components";
+import StButton from "../../Global/StButton/StButton";
 import { StModalContainer, StModalMain } from "../../Global/StModal/StModal";
 import deletePost from "../../service/postData/deletePost";
 import { Theme } from "../../style/Theme";
 
+const StMenuList = styled.ul`
+  width: 100%;
+  flex-wrap: wrap;
+  display: flex;
+  justify-content: center;
+  align-items: cetner;
+`;
+
+const StMenuItem = styled.li`
+  width: 100%;
+  height: 100%;
+  padding: 1em;
+  border-bottom: 1px solid ${Theme.colors.borderColor};
+  text-align: center;
+  transition: all 300ms ease-in;
+
+  &:hover {
+    background-color: ${Theme.colors.backgroundColor};
+  }
+
+  & > :hover {
+    font-weight: 600;
+  }
+`;
 const PostMenu = ({
   handlePostMenu,
   clickedPostId,
   clickedPostUid,
   currentUserUid,
 }) => {
+  const myPostMenuList = [
+    { text: "삭제", index: 1, ftn: "del" },
+    { text: "게시물로 이동", index: 2, ftn: "" },
+    { text: "링크 복사", index: 3, ftn: "" },
+    { text: "취소", index: 4, ftn: "cancel" },
+  ];
+
+  const userPostMenuList = [
+    { text: "신고", index: 1, ftn: "" },
+    { text: "팔로우 취소", index: 2, ftn: "" },
+    { text: "게시물 이동", index: 3, ftn: "" },
+    { text: "링크복사", index: 4, ftn: "" },
+    { text: "취소", index: 5, ftn: "cancel" },
+  ];
+
   const delPost = () => {
     deletePost(clickedPostId);
   };
+
+  const selectFnc = (ftn) => {
+    console.log(ftn);
+    if (ftn === "del") {
+      delPost();
+    } else if (ftn === "cancel") {
+      handlePostMenu();
+    } else if (ftn === "") {
+      alert("준비중입니다...");
+    }
+  };
+
   return (
     <StModalContainer
       display="flex"
@@ -20,26 +73,47 @@ const PostMenu = ({
     >
       <StModalMain
         width="25em"
-        height="30em"
+        height="auto"
         bgColor={Theme.colors.contentColor}
         border={`1px solid ${Theme.colors.borderColor}`}
         borderRadius={Theme.colors.borderRadius}
       >
         {currentUserUid === clickedPostUid ? (
-          <ul>
-            <li>
-              <button onClick={delPost}>삭제</button>
-            </li>
-            <li>
-              <button onClick={handlePostMenu}>취소</button>
-            </li>
-          </ul>
+          <StMenuList>
+            {myPostMenuList.map((item) => (
+              <StMenuItem
+                key={item.index}
+                onClick={() => {
+                  selectFnc(item.ftn);
+                }}
+              >
+                <StButton
+                  width="100%"
+                  height="100%"
+                  btnText={item.text}
+                  fontSize="1em"
+                />
+              </StMenuItem>
+            ))}
+          </StMenuList>
         ) : (
-          <ul>
-            <li>
-              <button onClick={handlePostMenu}>취소</button>
-            </li>
-          </ul>
+          <StMenuList>
+            {userPostMenuList.map((item) => (
+              <StMenuItem
+                key={item.index}
+                onClick={() => {
+                  selectFnc(item.ftn);
+                }}
+              >
+                <StButton
+                  width="100%"
+                  height="100%"
+                  btnText={item.text}
+                  fontSize="1em"
+                />
+              </StMenuItem>
+            ))}
+          </StMenuList>
         )}
       </StModalMain>
     </StModalContainer>
