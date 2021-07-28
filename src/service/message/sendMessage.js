6c2fe7bcd4bb22186706012fb8930534d1b030dc
csjv95 <csjv95@gmail.com>
2021-07-28 23:48:47 +0900

    I record that (id,time,message) when use this post message

--- src/service/message/sendMessage.js
+++ src/service/message/sendMessage.js
@@ -1,29 +1,29 @@
-import axios from "axios";
-
-const sendMessage = () => {
-  const result = axios({
-    url: "https://content-fcm.googleapis.com/v1/projects/477003740312/messages:send?",
-    method: "post",
+const sendMessage = (message, token) => {
+  //ajax 클라이언트단 node로 집적 통신 하면 괜찮아진다
+  const request = require("request");
+  const options = {
+    uri: "https://fcm.googleapis.com/fcm/send",
+    method: "POST",
     headers: {
-      Authorization:
-        "Bearer key",
-      Accept: "application/json",
       "content-type": "application/json",
+      Authorization: process.env.REACT_APP_FIREBASE_MESSAGEING_KEY,
     },
-    message: {
-      token:
-        "token",
+    json: {
+      to: token,
       notification: {
-        title: "you got a message",
-        body: "hello yong men",
-      },
-      webpush: {
-        fcm_options: {
-          link: "https://dummypage.com",
-        },
+        title: "you got a new massage",
+        message,
       },
     },
+  };
+  request.post(options, function (err, httpResponse, body, request) {
+    const time = new Date(httpResponse.headers.date); //메세지 gmt를 한국시간으로 바꾸기
+    const message = JSON.parse(httpResponse.request.body);
+    console.log("----------header input test-------------");
+    // console.log(httpResponse);
+    console.log("id", body.multicast_id); // 메세지 아이디
+    console.log("time", time); //메시지 시간
+    console.log("message", message.notification.message); //
   });
-  console.log(result);
 };
 export default sendMessage;
