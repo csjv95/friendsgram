3e9e18bff243fab76c16332e92e1d2c06725b883
csjv95 <csjv95@gmail.com>
2021-08-08 22:40:47 +0900

    send message to firebase store

--- src/service/message/sendMessage.js
+++ src/service/message/sendMessage.js
@@ -1,7 +1,19 @@
-import { firebaseStore } from '../firebase'
+import { firebaseAuth, firebaseStore } from '../firebase'
 
-const sendMessage = (text) => {
-  firebaseStore.collection('chatRooms')
+const sendMessage = async (text, clickedRoomId) => {
+  const uid = firebaseAuth.currentUser.uid
+  const time = Date.now()
+  console.log(clickedRoomId)
+  await firebaseStore
+    .collection('chatRooms')
+    .doc(clickedRoomId)
+    .collection('message')
+    .doc()
+    .set({
+      text,
+      uid,
+      time,
+    })
 }
 
 export default sendMessage
